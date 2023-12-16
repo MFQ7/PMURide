af5392451a5748f4d62b0559d7b3effc2d20497d
barhoom-22 <117360224+barhoom-22@users.noreply.github.com>
2023-12-16 14:01:11 +0300

    Update Ride.java
    Added cost and estimated time

--- src/Ride.java
+++ src/Ride.java
@@ -2,13 +2,16 @@ public class Ride {
     private Driver driver;
     private Passenger passenger;
     private float distance;
-
+    private float cost;
+    private int estimatedTime;
 
     public Ride(Driver driver, Passenger passenger) {
         this.driver = driver;
         this.passenger = passenger;
         this.distance = passenger.getLocation().distanceDifference(passenger.getDestination());
-
+        this.cost = 10 + distance * 2;
+        float driverPassengerDis = passenger.getLocation().distanceDifference(driver.getLocation());
+        this.estimatedTime = (int) Math.round(driverPassengerDis * 1.5);
     }
 
 
@@ -36,5 +39,20 @@ public class Ride {
         this.distance = passenger.getLocation().distanceDifference(passenger.getDestination());
     }
 
+    public float getCost() {
+        return cost;
+    }
 
+    public void setCost(float cost) {
+        this.cost = 10 + distance * 2;
+    }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public void setEstimatedTime(int estimatedTime) {
+        float driverPassengerDis = passenger.getLocation().distanceDifference(driver.getLocation());
+    }    
 }
+    
